f07420f3598828bd2448187e201f6f58c2a9df0d
Ludovic Lerus <lerus_l@epitech.eu>
2019-05-28 21:56:21 +0200

    feat(plugin): First version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fastify-bull
-Fastify plugin to use easily Bull
+# @guivic/fastify-bull
+[![dependencies status](https://david-dm.org/guivic/fastify-bull/status.svg)](https://david-dm.org/guivic/fastify-bull#info=dependencies)
+[![dev-dependencies status](https://david-dm.org/guivic/fastify-bull/dev-status.svg)](https://david-dm.org/guivic/fastify-bull#info=devDependencies)
+[![Build Status](https://travis-ci.org/guivic/fastify-bull.svg?branch=master)](https://travis-ci.org/guivic/fastify-bull)
+[![npm version](https://badge.fury.io/js/%40guivic%2Ffastify-bull-guivic.svg)](https://badge.fury.io/js/%40guivic%2Ffastify-bull-guivic)
+
+## Installation
+
+```bash
+npm install @guivic/fastify-bull --save
+
+yarn add @guivic/fastify-bull
+```
+
+## Usage
+
+COMING SOON
+
+## Options
+
+COMING SOON
+
+## Author
+
+Ludovic Lérus from [Guivic](https://guivic.io)
+
+## License
+
+Licensed under [MIT](./LICENSE).
---
+++ index.js
@@ -0,0 +1,82 @@
+const fastifyPlugin = require('fastify-plugin');
+const fastGlob = require('fast-glob');
+const path = require('path');
+const Queue = require('bull');
+const Redis = require('ioredis');
+
+/**
+ * Default on failed handler
+ * @param {Object} fastify - Fastify instance
+ * @param {Object} error - The Bull error
+ */
+function defaultOnFailed(fastify, error) {
+	fastify.log.error(`${error.queue.name}`);
+	fastify.log.error(error);
+}
+
+/**
+ * Default on error handler
+ * @param {Object} fastify - Fastify instance
+ * @param {Object} error - The Bull error
+ */
+function defaultOnError(fastify, error) {
+	fastify.log.error(`${error.queue.name}`);
+	fastify.log.error(error);
+}
+
+/**
+ * Load every queue file and decorate the Fastify instance.
+ * @param {Object} fastify - Fastify instance
+ * @param {Object} options - Plugin's options
+ * @param {Function} next - Fastify next callback
+ */
+function fastifyBull(fastify, options, next) {
+	const files = fastGlob.sync(options.paths);
+	const queues = {};
+
+	const client = new Redis(options.redisUrl);
+	const subscriber = new Redis(options.redisUrl);
+
+	const queueDefaultOptions = {
+		createClient(type) {
+			switch (type) {
+			case 'client':
+				return client;
+			case 'subscriber':
+				return subscriber;
+			default:
+				return new Redis();
+			}
+		},
+	};
+
+	if (!options.onFailed) {
+		options.onFailed = defaultOnFailed;
+	} if (!options.onError) {
+		options.onError = defaultOnError;
+	}
+
+	for (let i = 0; i < files.length; i++) {
+		const queueConfig = require(path.resolve(files[i]));
+		const filename = path.basename(files[i]);
+		const queueName = filename.split('.')[0];
+
+		queues[queueName] = new Queue(
+			queueConfig.name,
+			queueConfig.url || options.url,
+			queueConfig.options || queueDefaultOptions,
+		);
+
+		queues[queueName].process((job) => queueConfig.handler(fastify, job));
+
+		queues[queueName].on('error', (error) => options.onError(fastify, error));
+		queues[queueName].on('failed', (error) => options.onFailed(fastify, error));
+	}
+
+	fastify.decorate('queues', queues);
+	next();
+}
+
+module.exports = fastifyPlugin(fastifyBull, {
+	name: 'fastify-bull',
+});
