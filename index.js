d3da4793ebdff470ecef0bbbc369d5e380ec4860
Jerry Thomas <me@jerrythomas.name>
2020-04-14 12:32:31 +0530

    Removed dependency on fast-glob. Added usage instructions.

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const fastifyPlugin = require('fastify-plugin');
-const fastGlob = require('fast-glob');
+const fs = require('fs');
 const path = require('path');
 const Queue = require('bull');
 const Redis = require('ioredis');
+
+/**
+ * Walk folder path to find queue handlers
+ *
+ * @param {String} handlerPath - Folder containing the queue handlers
+ * @param {String} filter - Filter condition to restrict the types of files
+ */
+function walk(handlerPath, filter='js$') {
+  let filepaths = []
+
+  const files = fs.readdirSync(handlerPath);
+  for (let filename of files) {
+    const filepath = path.join(handlerPath, filename);
+    let ext = path.extname(filename)
+
+    if (fs.statSync(filepath).isDirectory()) {
+      filepaths = filepaths.concat(walk(filepath, filter));
+    } else if (ext.match(filter) !== null){
+      filepaths.push(filepath);
+    }
+  }
+
+  return filepaths;
+}
+
+/**
+ * Find list of files containing queue handlers
+ *
+ * @param {Object} fastify - Fastify instance
+ * @param {Object} options - Plugin's options containing path and prefix array
+ */
+function findQueueFiles(fastify, options){
+  let handlerPaths = []
+
+  options.prefix.forEach(prefix => {
+    handlerPath = path.resolve(`${prefix}/${options.path}`)
+    if (fs.existsSync(handlerPath) && fs.lstatSync(handlerPath).isDirectory()){
+      handlerPaths.push(handlerPath);
+    }
+  })
+  if (handlerPaths.length > 0){
+    return walk(handlerPaths[0])
+  }
+  return []
+}
 
 /**
  * Default on failed handler
@@ -10,8 +55,8 @@ const Redis = require('ioredis');
  * @param {Object} error - The Bull error
  */
 function defaultOnFailed(fastify, error) {
-	fastify.log.error(`${error.queue.name}`);
-	fastify.log.error(error);
+  fastify.log.error(`${error.queue.name}`);
+  fastify.log.error(error);
 }
 
 /**
@@ -20,8 +65,8 @@ function defaultOnFailed(fastify, error) {
  * @param {Object} error - The Bull error
  */
 function defaultOnError(fastify, error) {
-	fastify.log.error(`${error.queue.name}`);
-	fastify.log.error(error);
+  fastify.log.error(`${error.queue.name}`);
+  fastify.log.error(error);
 }
 
 /**
@@ -31,48 +76,51 @@ function defaultOnError(fastify, error) {
  * @param {Function} next - Fastify next callback
  */
 function fastifyBull(fastify, options, next) {
-	const files = fastGlob.sync(options.paths);
-	const queues = {};
+  options.prefix = options.prefix || ['.', 'dist', 'src']
+  options.path = options.path || 'queues'
 
-	const client = new Redis(options.redisUrl);
-	const subscriber = new Redis(options.redisUrl);
+  const files = findQueueFiles(fastify, options);
+  const queues = {};
+  const client = new Redis(options.redisUrl);
+  const subscriber = new Redis(options.redisUrl);
 
-	const queueOptions = {
-		createClient(type) {
-			switch (type) {
-			case 'client': return client;
-			case 'subscriber': return subscriber;
-			default: return new Redis(options.redisUrl);
-			}
-		},
-	};
+  //console.log(walk(options.paths))
+  const queueOptions = {
+    createClient(type) {
+      switch (type) {
+      case 'client': return client;
+      case 'subscriber': return subscriber;
+      default: return new Redis(options.redisUrl);
+      }
+    },
+  };
 
-	if (!options.onFailed) {
-		options.onFailed = defaultOnFailed;
-	} if (!options.onError) {
-		options.onError = defaultOnError;
-	}
+  if (!options.onFailed) {
+    options.onFailed = defaultOnFailed;
+  } if (!options.onError) {
+    options.onError = defaultOnError;
+  }
 
-	for (let i = 0; i < files.length; i++) {
-		const queueConfig = require(path.resolve(files[i]));
-		const filename = path.basename(files[i]);
-		const queueName = filename.split('.')[0];
+  for (let i = 0; i < files.length; i++) {
+    const queueConfig = require(path.resolve(files[i]));
+    const filename = path.basename(files[i]);
+    const queueName = queueConfig.name;
 
-		queues[queueName] = new Queue(
-			queueConfig.name,
-			queueOptions,
-		);
+    queues[queueName] = new Queue(
+      queueConfig.name,
+      queueOptions,
+    );
 
-		queues[queueName].process((job) => queueConfig.handler(fastify, job));
+    queues[queueName].process((job) => queueConfig.handler(fastify, job));
 
-		queues[queueName].on('error', (error) => options.onError(fastify, error));
-		queues[queueName].on('failed', (error) => options.onFailed(fastify, error));
-	}
+    queues[queueName].on('error', (error) => options.onError(fastify, error));
+    queues[queueName].on('failed', (error) => options.onFailed(fastify, error));
+  }
 
-	fastify.decorate('queues', queues);
-	next();
+  fastify.decorate('queues', queues);
+  next();
 }
 
 module.exports = fastifyPlugin(fastifyBull, {
-	name: 'fastify-bull',
+  name: 'fastify-bull',
 });
